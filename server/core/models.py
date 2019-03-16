db224ba9c053ceba37aada00c844544e309713a2
Mihsi <lascu.mihai.teodor@gmail.com>
2019-03-16 13:55:14 +0200

    Added backend functionality. User create/login/manage apis

---
+++ server/core/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+class Portfolio(models.Model):
+    """Model for Portfolio"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    portfolio_name = models.CharField(max_length=50, default="Crypto portfolio")
+
+    def __str__(self):
+        return self.portfolio_name
+
+
+class Cryptocurrency(models.Model):
+    """Model for Cryptocurrencies"""
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    coin_symbol = models.CharField(max_length=10)
+    coin_amount = models.FloatField(default=0.0)
+    coin_investment = models.FloatField(default=0.0)
